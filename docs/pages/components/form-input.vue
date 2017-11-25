5ac9f9e386ef083698d5a385afa123945ffab97a
Michał Mieluch <mieluch@gmail.com>
2017-11-25 16:36:02 +0000

    Put content in smaller cell

--- docs/pages/components/form-input.vue
+++ docs/pages/components/form-input.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div class="row">
+    <div class="grid-x">
       <div class="cell">
         <h1>Form Input</h1>
       </div>
     </div>
-    <div class="row">
-      <div class="cell">
+    <div class="grid-x">
+      <div class="cell small-12 medium-8 large-6">
         <FormInput />
       </div>
     </div>
