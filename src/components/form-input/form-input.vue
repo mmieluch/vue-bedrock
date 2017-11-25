c78f2417c0e8e5d9ed063588c30bebed0f12cdf6
Michał Mieluch <mieluch@gmail.com>
2017-11-25 15:51:51 +0000

    Add FormInput component

---
+++ src/components/form-input/form-input.vue
@@ -0,0 +1,20 @@
+<template>
+  <input />
+</template>
+
+<script>
+  // Supported input types.
+  const SUPPORTED_TYPES = [
+    'text', 'date', 'datetime', 'datetime-local', 'email', 'month',
+    'number', 'password', 'search', 'tel', 'time', 'url', 'week',
+  ]
+
+  export default {
+    name: 'FormInput',
+    props: {
+      value: {
+        default: null,
+      },
+    },
+  }
+</script>
