b8bbdb04e201f103c6fb3cc7be34598121874b5a
Michał Mieluch <mieluch@gmail.com>
2017-11-25 13:23:46 +0000

    Fix Nuxt config

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,41 +1,15 @@
 module.exports = {
   srcDir: __dirname,
 
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#2c3840' },
-  /*
-  ** Build configuration
-  */
   build: {
     extractCSS: true,
     cssSourceMap: true,
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
+    extend (config) {
       config.resolve.alias.vue = 'vue/dist/vue.common'
 
       config.devtool = 'source-map'
-
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-        })
-      }
-    },
+    }
   },
 
-  manifest: {
-    name: 'Vue Bedrock',
-    description: 'Foundation components for Vue.js',
-  },
-
-  css: [
-    'foundation-sites/dist/css/foundation.css',
-  ],
+  css: [],
 }
