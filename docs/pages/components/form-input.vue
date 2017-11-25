d6af088c77447a5ed4a914e3bde63bfa5b6a46d3
Michał Mieluch <mieluch@gmail.com>
2017-11-25 15:52:07 +0000

    Add a page for FormInput component

---
+++ docs/pages/components/form-input.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="cell">
+        <h1>Form Input</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="cell">
+        <FormInput />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import FormInput from '~~/src/components/form-input'
+
+  export default {
+    name: 'Page--FormInput',
+    components: { FormInput },
+  }
+</script>
