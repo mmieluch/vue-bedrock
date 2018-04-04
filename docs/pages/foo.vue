2625f39024feda6087b811a5ae8294c140169572
Michal Mieluch <michal.mieluch@bluefroglondon.com>
2018-04-04 16:44:46 +0100

    Add Foo page

---
+++ docs/pages/foo.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <Callout />
+  </article>
+</template>
+
+<script>
+  import Callout from '../../src/components/callout'
+
+  export default {
+    name: 'Foo',
+    layout: 'default',
+    components: {
+      Callout,
+    },
+  }
+</script>
