89fc6d799264ffbd0a5aa86ad83069bca8497512
Michal Mieluch <michal.mieluch@bluefroglondon.com>
2018-04-12 11:40:33 +0100

    Start writing dropdown-type menu items

--- docs/pages/foo.vue
+++ docs/pages/foo.vue
@@ -6,6 +6,10 @@
           <vb-menu>
             <vb-menu-item :to="to">One</vb-menu-item>
             <vb-menu-text>Text</vb-menu-text>
+            <vb-menu-item-dropdown>
+              <vb-menu-item :to="to">One</vb-menu-item>
+              <vb-menu-text>Text</vb-menu-text>
+            </vb-menu-item-dropdown>
           </vb-menu>
         </div>
       </div>
@@ -17,6 +21,7 @@
   import vbMenu from '../../src/components/navigation/menu/menu'
   import vbLink from '../../src/components/controls/link/link'
   import vbMenuItem from '../../src/components/navigation/menu/menu-item'
+  import vbMenuItemDropdown from '../../src/components/navigation/menu/menu-item-dropdown'
   import vbMenuText from '../../src/components/navigation/menu/menu-text'
 
   export default {
@@ -26,6 +31,7 @@
       vbLink,
       vbMenu,
       vbMenuItem,
+      vbMenuItemDropdown,
       vbMenuText,
     },
     data () {
